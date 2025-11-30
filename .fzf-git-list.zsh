726e45de267ce0b56abbdf08d04ee19d63b5d57d
ohikkoshi <21327096+ohikkoshi@users.noreply.github.com>
2025-11-30 20:48:35 +0900

    update fzf-git-list

--- .fzf-git-list.zsh
+++ .fzf-git-list.zsh
@@ -1,10 +1,68 @@
 # fzf:https://github.com/junegunn/fzf
 # ripgrep:https://github.com/BurntSushi/ripgrep
 # gnu-sed:https://www.gnu.org/software/sed/
-function fzf_git_list()
+fzf_git_list()
 {
-	local branch
-	
-	branch=$(git branch --all | rg -v HEAD | fzf +m) &&
-	git switch $(echo "$branch" | awk '{print $NF}' | sed 's#remotes/[^/]*/##') > /dev/null 2>&1
+	local selection
+	local tmp1="/tmp/fzf_git_lb_$$"
+	local tmp2="/tmp/fzf_git_rb_$$"
+	local tmp3="/tmp/fzf_git_lt_$$"
+	trap 'rm -f "$tmp1" "$tmp2" "$tmp3" 2>/dev/null' EXIT
+
+	# git branch
+    git branch --format='%(refname:short)' > "$tmp1" &
+    git branch -r 2>/dev/null | rg -v 'HEAD' | awk '{print $NF}' > "$tmp2" &
+    git tag --list > "$tmp3" &
+    wait
+
+	local local_branches=$(cat "$tmp1")
+	local remote_branches=$(cat "$tmp2")
+	local local_tags=$(sed 's/^/tags\//' "$tmp3")
+	local remote_tags=""
+
+	# support for remote tags
+	#remote_tags=$(
+	#	git ls-remote --tags origin 2>/dev/null |
+	#	awk '$2 !~ /\^{}$/ {sub("refs/tags/", "", $2); print $2}' |
+	#	sort -u |
+	#	comm -13 <(sort "$tmp3") - 2>/dev/null |
+	#	sed 's/^/origin\/tags\//'
+	#)
+
+	# fzf
+	selection=$(
+		printf "%s\n%s\n%s\n%s" \
+		"$local_branches" "$remote_branches" "$local_tags" "$remote_tags" |
+		rg -v '^$' |
+		fzf --prompt="git checkout > " +m
+	)
+    
+	# cancel
+	rm -f "$tmp1" "$tmp2" "$tmp3" 2>/dev/null
+	[[ -z "$selection" ]] && return 0
+
+	# git switch
+	case "$selection" in
+		tags/*|origin/tags/*)
+			local tag_name="${selection#tags/}"
+			tag_name="${tag_name#origin/}"
+
+			local new_branch="feature/$tag_name"
+			echo "tags/$tag_name → $new_branch"
+
+			if git show-ref --quiet "refs/heads/$new_branch"; then
+				git switch "$new_branch"
+			else
+				git switch -c "$new_branch" "$tag_name"
+			fi
+			;;
+		origin/*|*/*)
+			local branch="${selection#*/}"
+			git switch "$branch" 2>/dev/null || \
+				git switch -c "$branch" --track "$selection"
+			;;
+		*)
+			git switch "$selection"
+			;;
+	esac
 }
